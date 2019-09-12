aa633bda69da4b80e06f0b91f361c26dd67d2e3b
Rezaul Karim shaon <rkshaon.ist@gmail.com>
2019-09-13 03:27:42 +0600

    template added

--- djangoproject/enrollment/enroll/views.py
+++ djangoproject/enrollment/enroll/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    # return HttpResponse('Hello from Enrollment Home')
+    return render(request, 'index.html')
